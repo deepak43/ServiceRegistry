d083dc6d7a9e2e11adf9bdda2041fd872e04448a
Deepak Vaidya <deepak@Deepaks-MacBook-Pro.local>
2022-12-23 01:35:58 +0530

    First commit

---
+++ src/test/java/com/techailez/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techailez.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
